f2c99ee422d206610f212aebd9323701cca31a76
Minhaj Ahmed <minhaj@Minhajs-MacBook-Air.local>
2020-05-03 02:30:03 +0530

    Updates

---
+++ precache-manifest.e838aad45631ec531a8b840ab02f4b11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb23c5e8f85ec8f8bb51e6f3760bceab",
+    "url": "/doctorstrange/index.html"
+  },
+  {
+    "revision": "6771a30ca89a1499d6e5",
+    "url": "/doctorstrange/static/css/main.abae19d5.chunk.css"
+  },
+  {
+    "revision": "91627b8312d1ccde34bb",
+    "url": "/doctorstrange/static/js/2.b9d06fb1.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/doctorstrange/static/js/2.b9d06fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6771a30ca89a1499d6e5",
+    "url": "/doctorstrange/static/js/main.bf3b63ae.chunk.js"
+  },
+  {
+    "revision": "a31aee67ea01485d18ea",
+    "url": "/doctorstrange/static/js/runtime-main.f3a55f00.js"
+  }
+]);
